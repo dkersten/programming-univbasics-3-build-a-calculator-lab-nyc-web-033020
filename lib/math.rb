cfccc307ff9b79aec79d7c793c0e6992f5c04708
Daniel Kersten <dkersten1994@gmail.com>
2020-03-04 12:15:52 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,21 +4,26 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  difference = num1 - num2
+  difference
 end
 
 def division(num1, num2)
-
+  quotient = num1 / num2
+  quotient
 end
 
 def multiplication(num1, num2)
-
+  product = num1 * num2
+  product
 end
 
 def modulo(num1, num2)
-
+  mod = num1 % num2
+  mod
 end
 
 def square_root(num)
-
+  sqroot = sqrt(num)
+  sqroot
 end
